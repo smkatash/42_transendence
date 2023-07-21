3709235db1240ddcf5480bbe528504bc7b43f55f
Kanykei Tashbaeva <kanykei.tashbaeva@xl2.de>
2023-07-21 17:53:44 +0200

    feat: updaze

---
+++ backend/ping-pong-backend/src/Constants.ts
@@ -0,0 +1,3 @@
+export const CLIENT_ID='u-s4t2ud-88f48dd717740f9beaf02272845f6e47bbe053c586153dcac033e44b856527d8'
+export const CLIENT_SECRET='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'
+export const CALLBACK_URL='http://127.0.0.1:3000/42auth/redirect'
