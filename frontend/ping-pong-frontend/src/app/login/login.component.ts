7bd1c50703c90d49009a86a4de768ca5c2c81629
Jad Maalouf <jad2maalouf@gmail.com>
2023-08-01 11:17:17 +0200

    [in-progress]: login button refinement

--- frontend/ping-pong-frontend/src/app/login/login.component.ts
+++ frontend/ping-pong-frontend/src/app/login/login.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  printClick(): void {
+    console.log("Successfully clicked");
+  }
 }
