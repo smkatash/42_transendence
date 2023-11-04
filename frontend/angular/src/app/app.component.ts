6eb52c87fbcf2be4e84e8b428a1981932cf4359e
Jad Maalouf <jad2maalouf@gmail.com>
2023-11-04 14:29:37 +0100

    detecting routing changes

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { GameSocket } from './app.module';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, pipe } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Le Pong';
+  constructor(private router: Router, private socket: GameSocket) {}
+  ngOnInit(): void {
+    this.router.events
+      .subscribe((e) => {
+        if (e instanceof NavigationEnd) {
+          console.log(e.url)
+          if (e.url.includes('game')) {
+            this.socket.emit('router', 'game')
+          } else if (e.url.includes('leaderboard')) {
+            this.socket.emit('router', 'leaderboard')
+          } else if (e.url.includes('profile')) {
+            this.socket.emit('router', 'profile')
+          } else if (e.url.includes('chat')) {
+            this.socket.emit('router', 'chat')
+          }
+        }
+      })
+  }
 }
