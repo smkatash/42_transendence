837ed5b03143605f9c51dcb7bfdf7fbf59c9a2cc
Lars Krabbe <lkrabbe@3-C-2.42heilbronn.de>
2023-11-07 19:50:57 +0100

    update not finish

--- frontend/angular/src/app/chat/direct-message/direct-message.component.ts
+++ frontend/angular/src/app/chat/direct-message/direct-message.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { User } from 'src/app/entities.interface';
+import { ProfileService } from 'src/app/profile/profile.service';
+import { Observable, debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-direct-message',
@@ -23,12 +27,42 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   ],
 })
 export class DirectMessageComponent {
-  constructor(private chatservice: ChatService,
-    ){}
+  constructor(
+    private chatService: ChatService,
+    private profileService: ProfileService
+    ){};
 
-  @Input() isOpen?: boolean;
-  @Output() isOpenChange = new EventEmitter<boolean>
-  messageToSend?: string;
+    @Input() isOpen?: boolean;
+    @Output() isOpenChange = new EventEmitter<boolean>
+    dd_open?: boolean=false;
+    messageToSend?: string;
+    users$: Observable<User[]> = this.chatService.getChannelUsers()
+    currentUser?: User
+    targetUser?:User;
+    somePlaceholder:string = "Find the User"
+    UserSearch = new FormControl()
+    searchedUsers: User[]
+    var1: string = "";
+
+  ngOnInit() {
+    this.profileService.getCurrentUser()
+      .subscribe(user => this.currentUser = user)
+
+    this.UserSearch.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(
+        (username: string) => this.chatService.findUser(username)
+        .pipe(
+          tap((users: User[]) =>{
+            this.searchedUsers = users
+            console.log(users)
+          })
+        )
+      )
+    ).subscribe();
+
+  }
   
   toggle(): void {
     this.isOpen = !this.isOpen;
@@ -39,9 +73,23 @@ export class DirectMessageComponent {
     this.messageToSend = this.messageToSend.trim();
 
     if (!this.messageToSend) return;
-    this.chatservice.sendDM('userid',this.messageToSend);
+    this.chatService.sendDM('userid',this.messageToSend);
     this.messageToSend = ''
     this.toggle()
   }
+  selectUser(newUser:User)
+  {
+    // this.somePlaceholder=newUser.username;
+    this.targetUser=newUser;
+    this.var1 = newUser.username;
+    this.dd_open = true;
 
+  }
+  toggle_dropdown(): void {
+    this.dd_open = !this.dd_open;
+  }
+  resetInput()
+  {
+    this.UserSearch.reset();
+  }
 }
