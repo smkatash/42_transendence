6a15fd208bd1f0def33c6ecc9a111177d95e108c
Lars Krabbe <lkrabbe@1-E-13.42heilbronn.de>
2023-11-14 19:38:01 +0100

    fix the script

--- docker/replace_ng_env.sh
+++ docker/replace_ng_env.sh
@@ -8,4 +8,4 @@ value=$(echo "$line" | awk -F= '{print $2}')
 
 file="app/frontend/src/environments/environment.development.ts"
 
-sed -i'' -e "s/'$key:*'/'$key: $value'/api,/g" \"$file"
+awk -v key="$key" -v value="$value" '{gsub(key":.*", key": \047" value "\047,", $0); print}' "$file" > temp_file && mv temp_file "$file"
